37cb844bd7187d88c17f89742e5f40e401aea84c
Vachiravit Doungdee <mos14.d@gmail.com>
2024-05-19 10:11:54 +0700

    Update Certificateimage.jsx

--- src/app/[locale]/components/Certificateimage.jsx
+++ src/app/[locale]/components/Certificateimage.jsx
@@ -1,14 +1,17 @@
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
-export default function Certificateimage() {
+import 'react-awesome-slider/dist/styles.css'; // จำเป็นต้องเพิ่มไฟล์ CSS นี้ด้วย
 
+export default function CertificateImage() {
   return (
-    <AwesomeSlider animation="cubeAnimation">
-    <div data-src="/images/certficate-img/สื่อ.jpeg" />
-    <div data-src="/images/certficate-img/สื่อ2.jpeg" />
-    <div data-src="/images/certficate-img/สื่อ3.jpeg" />
-    <div data-src="/images/certficate-img/สื่อ4.jpeg" />
-    <div data-src="/images/certficate-img/สื่อ5.jpeg" />
-  </AwesomeSlider>
-  )
+    <div className="max-w-screen-lg mx-auto p-4">
+      <AwesomeSlider animation="cubeAnimation" className="h-full">
+        <div data-src="/images/certficate-img/สื่อ.jpeg" />
+        <div data-src="/images/certficate-img/สื่อ2.jpeg" />
+        <div data-src="/images/certficate-img/สื่อ3.jpeg" />
+        <div data-src="/images/certficate-img/สื่อ4.jpeg" />
+        <div data-src="/images/certficate-img/สื่อ5.jpeg" />
+      </AwesomeSlider>
+    </div>
+  );
 }
