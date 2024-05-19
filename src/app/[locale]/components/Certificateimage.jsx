7af42b983c48b2326f0553c4bf6d488a793e10f2
Vachiravit Doungdee <mos14.d@gmail.com>
2024-05-19 10:27:45 +0700

    Update Certificateimage.jsx

--- src/app/[locale]/components/Certificateimage.jsx
+++ src/app/[locale]/components/Certificateimage.jsx
@@ -4,11 +4,8 @@ import 'react-awesome-slider/dist/styles.css'; // จำเป็นต้อง
 
 export default function CertificateImage() {
   return (
-    <div className="max-w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl mx-auto p-2">
-      <AwesomeSlider 
-        animation="cubeAnimation" 
-        className="w-full h-64 sm:h-96 md:h-128 lg:h-160 xl:h-192 2xl:h-224"
-      >
+    <div className="max-w-screen-lg mx-auto p-2">
+      <AwesomeSlider animation="cubeAnimation" className="h-screen">
         <div data-src="/images/certficate-img/สื่อ.jpeg" />
         <div data-src="/images/certficate-img/สื่อ2.jpeg" />
         <div data-src="/images/certficate-img/สื่อ3.jpeg" />
